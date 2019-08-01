9660c0edcf3fd63ea5746f4608d3d08bdee8bc30
jaxon63 <jaxon63@gmail.com>
2019-08-01 10:22:52 +1000

    Update Pastime/Pastime/User.cs

---
+++ Pastime/Pastime/User.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pastime
+{
+    class User
+    {
+        private String email;
+        private String password;
+        private String username;
+
+        public User(string email, string password, string username)
+        {
+            this.email = email;
+            this.password = password;
+            this.username = username;
+        }
+
+        public void setEmail(String email)
+        {
+            this.email = email;
+        }
+
+        public String getEmail()
+        {
+            return email;
+        }
+
+        public void setPassword(String password)
+        {
+            this.password = password;
+        }
+
+        public String getPassword()
+        {
+            return password;
+        }
+
+        public void setUsername(String username)
+        {
+            this.username = username;
+        }
+
+        public String getUsername()
+        {
+            return username;
+        }
+
+        //TODO
+        public bool validateEmail()
+        {
+            return false;
+        }
+
+        //TODO
+        public bool validatePassword()
+        {
+            return false;
+        }
+    }
+}
