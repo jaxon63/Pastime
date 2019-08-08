8612672b988f60522cf9039654f6e26fae86a32e
jaxon63 <jaxon63@gmail.com>
2019-08-08 18:44:21 +1000

    Update User class

--- Pastime/Pastime/Models/User.cs
+++ Pastime/Pastime/Models/User.cs
@@ -1,62 +1,182 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace Pastime
 {
     class User
     {
-        private String email;
-        private String password;
-        private String username;
+        private int userId;
+        private string email;
+        private string password;
+        private string username;
+        private int yearJoined;
+        private string location;
+        private string faveSport;
+        private string bio;
+        private Uri dpUri;
+        private int rating;
 
-        public User(string email, string password, string username)
+        private Image displayPicture; //Don't include in database
+        
+
+        public User(int userId, string email, string password, string username, int yearJoined, string location, string faveSport, string bio, Uri dpUri, int rating)
         {
+            this.userId = userId;
             this.email = email;
             this.password = password;
             this.username = username;
+            this.yearJoined = yearJoined;
+            this.location = location;
+            this.faveSport = faveSport;
+            this.bio = bio;
+            this.dpUri = dpUri;
+            this.rating = rating;
+
+            getPictureFromUri();
         }
 
-        public void setEmail(String email)
+        public int UserId
         {
-            this.email = email;
+            get
+            {
+                return userId;
+            }
         }
 
-        public String getEmail()
+        public string Email
         {
-            return email;
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
         }
 
-        public void setPassword(String password)
+        public string Password
         {
-            this.password = password;
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
         }
 
-        public String getPassword()
+        public string Username
         {
-            return password;
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
         }
 
-        public void setUsername(String username)
+        public int YearJoined
         {
-            this.username = username;
+            get
+            {
+                return yearJoined;
+            }
         }
 
-        public String getUsername()
+        public string Location
         {
-            return username;
+            get
+            {
+                return location;
+            }
+            set
+            {
+                location = value;
+            }
+        }
+
+        public string FaveSport
+        {
+            get
+            {
+                return faveSport;
+            }
+            set
+            {
+                faveSport = value;
+            }
+        }
+
+        public string Bio
+        {
+            get
+            {
+                return bio;
+            }
+            set
+            {
+                bio = value;
+            }
+        }
+
+        public Uri DisplayPicture
+        {
+            get
+            {
+                return dpUri;
+            }
+            set
+            {
+                dpUri = value;
+            }
+        }
+
+        public int Rating
+        {
+            get
+            {
+                return rating;
+            }
+            set
+            {
+                rating = value;
+            }
+        }
+
+        //TODO
+        public void getPictureFromUri()
+        {
+          
+        }
+
+        //TODO
+        public void getCurrentLocation()
+        {
+
+        }
+
+        //TODO
+        public bool validateBio()
+        {
+            return true;
         }
 
         //TODO
         public bool validateEmail()
         {
-            return false;
+            return true;
         }
 
         //TODO
         public bool validatePassword()
         {
-            return false;
+            return true;
         }
     }
 }
