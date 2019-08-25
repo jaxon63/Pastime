47d1dc74c4de237f082df5c179de09f2bc3375e2
Brian Nguyen <37164329+100629900@users.noreply.github.com>
2019-08-25 21:53:22 +1000

    Add files via upload

---
+++ Pastime/Pastime/Views/Register.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Pastime
+{
+
+    public partial class Register : ContentPage
+    {
+        public Register()
+        {
+            InitializeComponent();
+            //NavigationPage.SetHasBackButton(this, false);
+            Validate();
+        }
+
+        public List<string> Validate()
+        {
+            Entry email = this.FindByName<Entry>("Email");
+            Entry password = this.FindByName<Entry>("Password");
+            Entry verify_password = this.FindByName<Entry>("Verify_Password");
+            Entry name = this.FindByName<Entry>("Name");
+
+            request.AddParameter("email", email.Text);
+            request.AddParameter("password", password.Text);
+            request.AddParameter("verify_password", verify_password.Text);
+        }
+
+        async void Registration(object sender, EventArgs args)
+        {
+            
+        }
+
+        async void OnBackButtonPressed(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
