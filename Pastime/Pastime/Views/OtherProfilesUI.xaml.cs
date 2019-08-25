2ca3d96fff1c39ccf383862af23c5a0666e96280
Steven <steven.j.moe@gmail.com>
2019-08-25 20:14:16 +1000

    master updated

--- Pastime/Pastime/Views/OtherProfilesUI.xaml.cs
+++ Pastime/Pastime/Views/OtherProfilesUI.xaml.cs
@@ -19,10 +19,10 @@ namespace Pastime
             InitializeComponent();
 
             //User object for testing
-            User user = new User("hellO@steven.com", "password1", "steveny1");
+            //User user = new User("hellO@steven.com", "password1", "steveny1");
 
             //Set the binding context for Other User profiles and pass user information
-            this.BindingContext = new OtherProfilesViewModel(user.getUsername(), "I like cricket and other stuff I guess", "Member since: 2014", "Location: Hawthorn", "Favourite Sport: Cricket" );
+            //this.BindingContext = new OtherProfilesViewModel(user.getUsername(), "I like cricket and other stuff I guess", "Member since: 2014", "Location: Hawthorn", "Favourite Sport: Cricket" );
         }
         
     }
