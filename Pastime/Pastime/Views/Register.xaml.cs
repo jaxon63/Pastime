77a1606e19c51bbc5fd96b5e74e652a7b0ba70f2
Brian Nguyen <37164329+100629900@users.noreply.github.com>
2019-08-25 22:02:19 +1000

    Updated Register page with more code
    Added some of the registration API declarations

--- Pastime/Pastime/Views/Register.xaml.cs
+++ Pastime/Pastime/Views/Register.xaml.cs
@@ -22,8 +22,19 @@ namespace Pastime
             Entry email = this.FindByName<Entry>("Email");
             Entry password = this.FindByName<Entry>("Password");
             Entry verify_password = this.FindByName<Entry>("Verify_Password");
-            Entry name = this.FindByName<Entry>("Name");
-
+            Entry username = this.FindByName<Entry>("Name");
+            
+            //Hopefully the registration API is register.php, otherwise I'll need to change this
+            string register_api = "https://vietnguyen.me/pastime/register.php"
+            
+            //creating the new client opject
+            var client = new RestClient(register_api);
+            
+            //creating the request, as per Viet's comment, I'm using the POST method
+            var reqeust = new RestRequest(Method.POST);
+            
+            //adding all the parameters for the user (all the information that's input when they're registering)
+            request.AddParameter("username", username.Text);
             request.AddParameter("email", email.Text);
             request.AddParameter("password", password.Text);
             request.AddParameter("verify_password", verify_password.Text);
