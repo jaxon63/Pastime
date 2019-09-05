46b51c0f7a68dee669cee98414f3fd762cc38537
jaxon63 <jaxon63@gmail.com>
2019-09-05 10:19:06 +1000

    Added Activity class

---
+++ Pastime/Pastime/Models/Activity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pastime.Models
+{
+    class Activity
+    {
+        private string name;
+        private Uri iconImage;
+        private List<string> equipment;
+
+
+        public Activity(string name, Uri icon, List<string> equipment)
+        {
+            this.name = name;
+            this.iconImage = icon;
+            this.equipment = equipment;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public Uri IconImage
+        {
+            get
+            {
+                return iconImage;
+            }
+        }
+
+        public List<string> Equipment
+        {
+            get
+            {
+                return equipment;
+            }
+        }
+    }
+}
