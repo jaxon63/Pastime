75fdabebfc549a4de95934ad8e8941247013dbc1
Anthony0298 <20942691+Anthony0298@users.noreply.github.com>
2019-09-12 13:47:05 +1000

    Added Icons and improved CreateEvent UI

--- Pastime/Pastime/Views/CreateEvent.xaml.cs
+++ Pastime/Pastime/Views/CreateEvent.xaml.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Pastime.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Page1 : ContentPage
+    public partial class CreateEvent : ContentPage
     {
-        public Page1()
+        public CreateEvent()
         {
             InitializeComponent();
         }
