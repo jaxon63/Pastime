8f25f6d6378defbb5e9be06bc2cc99c849ec901a
Viet Nguyen <vietnguyengit@outlook.com>
2019-10-11 02:22:37 +1100

    dynamic events view

--- Pastime/Pastime/ViewModels/CreateEventViewModel.cs
+++ Pastime/Pastime/ViewModels/CreateEventViewModel.cs
@@ -90,7 +90,7 @@ namespace Pastime.ViewModels
         public const string GooglePlacesDetailPath = "https://maps.googleapis.com/maps/api/place/details/json?place_id={0}&fields=geometry&key={1}";
 
         //TODO: store the key on the server
-        public const string GooglePlacesApiKey = "AIzaSyC8YbDhYT-NWyQTRHagstP_d13b5IPbOOQ";
+        public const string GooglePlacesApiKey = "";
 
         //Event model used to handle all the business logic regarding events
         private readonly EventModel model;
