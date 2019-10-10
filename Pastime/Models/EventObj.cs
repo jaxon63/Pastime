ef5849f1275e0a86d8e12d5f6794c08b9d226b37
Viet Nguyen <vietnguyengit@outlook.com>
2019-10-11 02:29:09 +1100

    Dynamic Events View

---
+++ Pastime/Models/EventObj.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pastime
+{
+    public class EventJSONObj
+    {
+        public string eventID { get; set; }
+        public string name { get; set; }
+        public string activity { get; set; }
+        public string equipment { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string max_guests { get; set; }
+        public string description { get; set; }
+        public string date { get; set; }
+        public string end_time { get; set; }
+    }
+
+    //The login JSON response may include multiple objects
+    public class EventJSON
+    {
+        public List<EventJSONObj> Event { get; set; }
+    }
+
+    //The login JSON response may include multiple objects
+    /*
+    public class EventsJSON
+    {
+        public List<EventJSON> Events { get; set; }
+    }
+    */
+}
