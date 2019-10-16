b8b35643a0ce2234bc5030a2c2f639282d58f4a7
Steven <steven.j.moe@gmail.com>
2019-10-16 15:35:06 +1100

    iOS splash screen works, need to fix android version

--- Pastime/Pastime.Android/MainActivity.cs
+++ Pastime/Pastime.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Android.OS;
 
 namespace Pastime.Droid
 {
-    [Activity(Label = "Pastime", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Pastime", Icon = "@mipmap/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
